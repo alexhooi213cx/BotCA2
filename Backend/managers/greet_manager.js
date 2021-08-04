018dfb9a655d0eae13bb93399a1e682d972ee558
alexhooi213cx <ahkc.19@ichat.sp.edu.sg>
2021-08-04 21:20:43 +0800

    4

--- Backend/managers/greet_manager.js
+++
@@ -1,9 +0,0 @@
-/* const utils = require('../utils');
-const databaseManager = require('./db_manager');
-
-module.exports.greeting = function () {
-    if (error) return Promise.reject(error);
-    return databaseManager        
-        .greeting()
-        
-}; */
